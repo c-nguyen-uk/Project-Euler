958041946f8d87256df77c2616f02cf24f15a7b8
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-03-09 21:24:52 +0000

    Add files via upload

---
+++ Problem15.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Problem 15: Lattice paths
+https://projecteuler.net/problem=15
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to
+the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+"""
+import timeit
+
+# This function returns n!.
+
+def factorial(k):
+    product = 1
+    for i in range(1, k + 1):
+        product *= i
+    return product
+
+# This function uses combinatorics to return the number of routes through a
+# general m x n grid.
+# To see why this is true, note that for an m x n grid we must travel to the
+# right m times, and down n times, no matter what route we take. There are
+# (m + n)! ways to arrange (m + n) different items. However, in our case, m of
+# the items are right, and n of the items are down, so we need to remove these
+# m! and n! repetitions from our count.
+
+def routes(m, n):
+    return factorial(m + n)//(factorial(m) * factorial(n))
+
+# Printing the solution and ensuring that it completes within 1 minute.
+
+start = timeit.default_timer()
+print(routes(20, 20))
+stop = timeit.default_timer()
+print('Time:', stop - start)
