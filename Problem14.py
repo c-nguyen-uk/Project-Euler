5e7612c0db1c841c5cb020803e6343f86d6c3238
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-03-11 14:57:24 +0000

    Update Problem14.py

--- Problem14.py
+++ Problem14.py
@@ -20,7 +20,6 @@ Which starting number, under one million, produces the longest chain?
 
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
-
 import timeit
 
 # This function returns the length of a Collatz sequence starting with n.
