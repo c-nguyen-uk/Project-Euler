91a1f0197ad78f313486f1eda683a04d36644e22
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-03-11 21:06:42 +0000

    Update Problem17.py

--- Problem17.py
+++ Problem17.py
@@ -23,13 +23,13 @@ import timeit
 
 # The below are dictionaries of all relevant digits.
 
-units = {"1":"one", "2":"two", "3":"three", "4":"four", "5":"five",
-         "6":"six", "7":"seven", "8":"eight", "9":"nine"}
-elevens = {"11":"eleven", "12":"twelve", "13":"thirteen", "14":"fourteen",
-            "15":"fifteen", "16":"sixteen", "17":"seventeen", "18":"eighteen",
-            "19":"nineteen"}
-tens = {"20": "twenty", "30":"thirty", "40":"forty", "50":"fifty",
-        "60":"sixty", "70":"seventy", "80":"eighty", "90":"ninety"}
+units = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
+         "6": "six", "7": "seven", "8": "eight", "9": "nine"}
+elevens = {"11": "eleven", "12": "twelve", "13": "thirteen", "14": "fourteen",
+            "15": "fifteen", "16": "sixteen", "17": "seventeen",
+            "18": "eighteen", "19": "nineteen"}
+tens = {"20": "twenty", "30": "thirty", "40": "forty", "50": "fifty",
+        "60": "sixty", "70": "seventy", "80": "eighty", "90": "ninety"}
 
 # This function converts any integer from 1 to 1000 into text.
 # This can be generalised for greater ranges using similar logic. It is
