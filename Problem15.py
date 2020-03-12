3f9db29468da2d10ff88acc3f6c69049e9275084
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-03-12 16:26:30 +0000

    Update Problem15.py
    Improving adherence to PEP 8.

--- Problem15.py
+++ Problem15.py
@@ -3,8 +3,9 @@
 Problem 15: Lattice paths
 https://projecteuler.net/problem=15
 
-Starting in the top left corner of a 2×2 grid, and only being able to move to
-the right and down, there are exactly 6 routes to the bottom right corner.
+Starting in the top left corner of a 2×2 grid, and only being able to
+move to the right and down, there are exactly 6 routes to the bottom
+right corner.
 
 How many such routes are there through a 20×20 grid?
 """
@@ -18,13 +19,13 @@ def factorial(k):
         product *= i
     return product
 
-# This function uses combinatorics to return the number of routes through a
-# general m x n grid.
-# To see why this is true, note that for an m x n grid we must travel to the
-# right m times, and down n times, no matter what route we take. There are
-# (m + n)! ways to arrange (m + n) different items. However, in our case, m of
-# the items are right, and n of the items are down, so we need to remove these
-# m! and n! repetitions from our count.
+# This function uses combinatorics to return the number of routes
+# through a general m x n grid.
+# To see why this is true, note that for an m x n grid we must travel
+# to the right m times, and down n times, no matter what route we take.
+# There are (m + n)! ways to arrange (m + n) different items. However,
+# in our case, m of the items are right, and n of the items are down,
+# so we need to remove these m! and n! repetitions from our count.
 
 def routes(m, n):
     return factorial(m + n)//(factorial(m) * factorial(n))
