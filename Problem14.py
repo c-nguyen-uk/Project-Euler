096a97245b22f1de961f0a7a35bab95233eecc25
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-03-12 16:54:26 +0000

    Update Problem14.py
    Improving adherence to PEP 8.

--- Problem14.py
+++ Problem14.py
@@ -3,28 +3,32 @@
 Problem 14: Longest Collatz sequence
 https://projecteuler.net/problem=14
 
-The following iterative sequence is defined for the set of positive integers:
+The following iterative sequence is defined for the set of positive
+integers:
 
 n → n/2 (n is even)
 n → 3n + 1 (n is odd)
 
-Using the rule above and starting with 13, we generate the following sequence:
+Using the rule above and starting with 13, we generate the following
+sequence:
 
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
 
-It can be seen that this sequence (starting at 13 and finishing at 1) contains
-10 terms. Although it has not been proved yet (Collatz Problem), it is thought
-that all starting numbers finish at 1.
+It can be seen that this sequence (starting at 13 and finishing at 1)
+contains 10 terms. Although it has not been proved yet (Collatz
+Problem), it is thought that all starting numbers finish at 1.
 
 Which starting number, under one million, produces the longest chain?
 
-NOTE: Once the chain starts the terms are allowed to go above one million.
+NOTE: Once the chain starts the terms are allowed to go above one
+million.
 """
 import timeit
 
-# This function returns the length of a Collatz sequence starting with n.
-# Note also that if n is odd, then 3n+1 is even, so we can add 2 to the count
-# and instead compute the next element of the chain using (3n+1)/2.
+# This function returns the length of a Collatz chain starting with n.
+# Note also that if n is odd, then 3n+1 is even, so we can add 2 to the
+# count and instead compute the next element of the chain using
+# an input of (3n+1)/2.
 
 def collatz_seq(n):
     count = 1
@@ -39,9 +43,9 @@ def collatz_seq(n):
             n = (3*n + 1)//2
     return count
 
-# This function returns the starting number under k with the longest sequence. 
-# Note that since collatz_seq(n) = collatz_seq(n/2) + 1, for even n, we don't
-# need to compute any chains below k/2.
+# This function returns the starting number under k with the longest
+# sequence. Note that since collatz_seq(n) = collatz_seq(n/2) + 1, for
+# even n, we don't need to compute any chains below k/2.
 
 def longest_collatz_number(k):
     long_start_number = 1
