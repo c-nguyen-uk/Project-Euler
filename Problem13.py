edcf7f944da2811cd733f18f95e9fe209a424bb5
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-06-18 09:17:33 +0100

    Fix comments in code

--- Problem13.py
+++ Problem13.py
@@ -10,7 +10,7 @@ Work out the first ten digits of the sum of the following one-hundred
 """
 import timeit
 
-# Displaying the large numbers nicely as a list.
+# Displays the large numbers nicely as a list.
 
 large_number = """37107287533902102798797998220837590246510135740250
 46376937677490009712648124896970078050417018260538
@@ -113,7 +113,7 @@ large_number = """37107287533902102798797998220837590246510135740250
 20849603980134001723930671666823555245252804609722
 53503534226472524250874054075591789781264330331690""".replace("\n", ",")
 
-# Printing the solution and ensuring that it completes within 1 minute.
+# Prints the solution and ensures that it completes within 1 minute.
 
 start = timeit.default_timer()
 print(str(sum([int(i) for i in large_number.split(",")]))[:10])
