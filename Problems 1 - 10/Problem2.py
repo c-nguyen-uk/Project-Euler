6bec31ad7d19ecea407490e02a22baf436d2393d
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-09-13 21:54:17 +0100

    Fix spacing

--- Problems 1 - 10/Problem2.py
+++ Problems 1 - 10/Problem2.py
@@ -15,7 +15,6 @@ exceed four million, find the sum of the even-valued terms.
 import timeit
 
 # This function returns n Fibonacci terms, initialised with a and b.
-
 def fib(a, b, n):
     fib_list = [a, b]
     for i in range(2, n):
@@ -25,14 +24,12 @@ def fib(a, b, n):
 # This function returns Fibonacci terms up to a maximum limit L.
 # We use the fact that F_L > L, so the iterable range is never reached.
 # Note that in general this is not true, e.g. a = 0.1, b = 0.1.
-
 def fib_up_to(a, b, L):
     for i in range(L):
         if fib(a, b, i)[-1] > L:
             return fib(a, b, i)[:-1]
         
-# Prints the solution and ensures that it completes within 1 minute.
-
+# This prints the solution and the time to completion.
 start = timeit.default_timer()
 print(sum([i for i in fib_up_to(1, 2, 4000000) if i % 2 == 0]))
 stop = timeit.default_timer()
