58bf914c855692070f4ab105adab48c1ca494f82
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-10-13 09:40:43 +0100

    Fix comment spacing

--- Problems 11 - 20/Problem17.py
+++ Problems 11 - 20/Problem17.py
@@ -10,9 +10,9 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written
 out in words, how many letters would be used?
 
 
-NOTE: Do not count spaces or hyphens. For example, 342 (three hundred
+NOTE: Do not count spaces or hyphens.  For example, 342 (three hundred
 and forty-two) contains 23 letters and 115 (one hundred and fifteen)
-contains 20 letters. The use of "and" when writing out numbers is in
+contains 20 letters.  The use of "and" when writing out numbers is in
 compliance with British usage.
 """
 import timeit
