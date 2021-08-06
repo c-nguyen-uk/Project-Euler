966a1609aa14643e4311453dc2086d2d4532688c
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2021-08-06 14:40:36 +0100

    Implement timing decorator and refactor

--- Problems 11 - 20/Problem17.py
+++ Problems 11 - 20/Problem17.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-"""
-Problem 17: Number letter counts
+"""Problem 17: Number letter counts
+
 https://projecteuler.net/problem=17
 
 If the numbers 1 to 5 are written out in words: one, two, three, four,
@@ -14,61 +14,73 @@ NOTE: Do not count spaces or hyphens.  For example, 342 (three hundred
 and forty-two) contains 23 letters and 115 (one hundred and fifteen)
 contains 20 letters.  The use of "and" when writing out numbers is in
 compliance with British usage.
+
+Solver note:: This problem can also be solved by hand, but we focus on
+a more code-centric solution here.
 """
-import timeit
+from custom_timer import computation_time
 
-# This problem can also be solved by hand.
-# We focus on a more code-centric solution here.
 
-# The below are dictionaries of all relevant digits.
-units = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
-         "6": "six", "7": "seven", "8": "eight", "9": "nine"}
-elevens = {"11": "eleven", "12": "twelve", "13": "thirteen", "14": "fourteen",
-            "15": "fifteen", "16": "sixteen", "17": "seventeen",
-            "18": "eighteen", "19": "nineteen"}
-tens = {"20": "twenty", "30": "thirty", "40": "forty", "50": "fifty",
-        "60": "sixty", "70": "seventy", "80": "eighty", "90": "ninety"}
+UNITS = {
+    "1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
+    "6": "six", "7": "seven", "8": "eight", "9": "nine"
+    }
+ELEVENTEENS = {
+    "11": "eleven", "12": "twelve", "13": "thirteen", "14": "fourteen",
+    "15": "fifteen", "16": "sixteen", "17": "seventeen",
+    "18": "eighteen", "19": "nineteen"
+    }
+TENS = {
+    "20": "twenty", "30": "thirty", "40": "forty", "50": "fifty",
+    "60": "sixty", "70": "seventy", "80": "eighty", "90": "ninety"
+        }
 
-# This function converts any integer from 1 to 1000 into text.
-# We can generalise for greater ranges using similar logic.  It is
-# possible to refactor this code to avoid repeated logic, but one needs
-# to be careful to deal with any indexing issues.
+
 def number_to_text(n):
+    """Convert an integer in [1, 1000] into a string.
+
+    Note that this can be generalised to greater ranges using similar logic,
+    and that it is possible to refactor this code to avoid repetition, but
+    care needs to be taken with indexing issues.
+    """
     n = str(n)
     word = ""
     if len(n) == 1:  # Do 1-9
-        word += units[n[-1]]
+        word += UNITS[n[-1]]
     if len(n) == 2:
         if n[-2] == "1" and n[-1] == "0":  # Do 10
             word += "ten"
         elif n[-2] == "1" and n[-1] != "0":  # Do 11-19
-            word += elevens[n[-2:]]
+            word += ELEVENTEENS[n[-2:]]
         if n[-2] != "1" and n[-1] != "0":  # Do 21-99, not ending in 0
-            word += tens[n[-2] + "0"] + "-" + units[n[-1]]
+            word += TENS[n[-2] + "0"] + "-" + UNITS[n[-1]]
         elif n[-2] != "1" and n[-1] == "0":  # Do 20-90, ending in 0
-            word += tens[n[-2:]]
+            word += TENS[n[-2:]]
     if len(n) == 3:
         if n[-2] == "0" and n[-1] == "0":  # Do 100-900, ending in 00
-            word += units[n[-3]] + " hundred"
+            word += UNITS[n[-3]] + " hundred"
         elif n[-2] != "0" or n[-3] != "0":
-            word += units[n[0]] + " hundred and "
+            word += UNITS[n[0]] + " hundred and "
             if n[-2] == "1" and n[-1] != "0":  # Do 101-999, ending in 11-19
-                word += elevens[n[-2:]]
+                word += ELEVENTEENS[n[-2:]]
             elif n[-2] == "1" and n[-1] == "0":  # Do 101-999, ending in 10
                 word += "ten"
             if n[-2] != "1" and n[-2] != "0" and n[-1] != "0":
-                word += tens[n[-2] + "0"] + "-" + units[n[-1]]
+                word += TENS[n[-2] + "0"] + "-" + UNITS[n[-1]]
             elif n[-2] != "1" and n[-2] != "0" and n[-1] == "0":
-                word += tens[n[-2:]]
+                word += TENS[n[-2:]]
             elif n[-2] != "1" and n[-2] == "0":
-                word += units[n[-1]]
+                word += UNITS[n[-1]]
     if len(n) == 4:
         word += "one thousand"
     return word.title()
 
-# This prints the solution and the time to completion.
-start = timeit.default_timer()
-print(sum([len(number_to_text(i).replace(" ", "").replace("-", ""))
-           for i in range(1, 1001)]))
-stop = timeit.default_timer()
-print("Time:", stop - start)
+
+@computation_time
+def solution():
+    """Return the solution."""
+    return sum([len(number_to_text(i).replace(" ", "").replace("-", ""))
+                for i in range(1, 1001)])
+
+
+print(solution())
