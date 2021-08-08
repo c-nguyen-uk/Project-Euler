f3347bb74d8011d21639272467229d307b676e54
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2021-08-08 02:48:58 +0100

    Remove redundant comment block

--- Problems 11 - 20/Problem11.py
+++ Problems 11 - 20/Problem11.py
@@ -37,9 +37,6 @@ PROBLEM_GRID = str("""
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
 """.replace(" ", "").replace("\n", ""))
 
-# Creats a matrix out of the problem grid.
-# Matrix creation could have been done by hand, but is error-prone.
-
 
 def row(n):
     """Return the nth row of PROBLEM_GRID as a list."""
