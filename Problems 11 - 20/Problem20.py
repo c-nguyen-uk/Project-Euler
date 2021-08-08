47d9ba7bdb34218ffb5da9a2f47692a7760f109e
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2021-08-08 03:45:52 +0100

    Create Problem20.py

---
+++ Problems 11 - 20/Problem20.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Problem 20: Factorial digit sum
+
+https://projecteuler.net/problem=20
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is
+3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+from custom_timer import computation_time
+
+
+def factorial(n):
+    """Return n!."""
+    product = 1
+    for i in range(1, n + 1):
+        product *= i
+    return product
+
+
+@computation_time
+def solution(n):
+    """Return the solution, for n!."""
+    return sum([int(i) for i in list(str(factorial(n)))])
+
+
+print(solution(100))
